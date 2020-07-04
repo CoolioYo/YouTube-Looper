7a5581c0e9410ad416ad6bbba334c98cd9a69c4b
CoolioYo <georgiacmartinez@gmail.com>
2020-07-03 21:19:14 -0500

    Added loop full video option

--- popup.js
+++ popup.js
@@ -17,24 +17,40 @@ document.addEventListener('DOMContentLoaded', function() {
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+        // Get the video url
         videoId = link.replace("https://www.youtube.com/watch?v=", "");
         videoId = videoId.split("&")[0];
         console.log(videoId);
 
-        startSeconds = document.getElementById("start-time").value;
-        endSeconds = document.getElementById("end-time").value;
+        // Check what portion of video to loop
+        var checkbox = document.getElementById('checkbox');
 
-        startSeconds = findSeconds(startSeconds);
-        endSeconds = findSeconds(endSeconds);
+        if(checkbox.checked){
+            // Loop full video
+            player.loadVideoById({
+                videoId: videoId,
+                loop: 1
+            });
+            console.log("Looping full video")
 
-        player.loadVideoById({
-            videoId: videoId,
-            startSeconds: startSeconds,
-            endSeconds: endSeconds
-        });
+        }else{
+            // Loop part of a video
+            startSeconds = document.getElementById("start-time").value;
+            endSeconds = document.getElementById("end-time").value;
+
+            startSeconds = findSeconds(startSeconds);
+            endSeconds = findSeconds(endSeconds);
+
+            player.loadVideoById({
+                videoId: videoId,
+                startSeconds: startSeconds,
+                endSeconds: endSeconds
+            });
+        }
     });
 });
 
+// Convert time input to seconds
 function findSeconds(time){
     minutes = parseInt(time.split(':')[0] * 60);
     seconds = minutes + parseInt(time.split(':')[1]);
@@ -42,6 +58,7 @@ function findSeconds(time){
     return seconds;
 }
 
+// Video setup 
 function onYouTubePlayerAPIReady() {
     player = new YT.Player('ytplayer', {
         width: "320",
@@ -50,7 +67,6 @@ function onYouTubePlayerAPIReady() {
         playerVars: {
             autoplay: 1, // Auto-play the video on load
             controls: 1, // Show pause/play buttons in player
-            disablekb: 1, // Disable keyboard
             autohide: 0, // Hide video controls when playing
             start: startSeconds,
             end: endSeconds,
@@ -71,3 +87,24 @@ function onStateChange(state) {
         player.seekTo(startSeconds)
     }
 }
+
+// Checkbox input
+document.addEventListener('DOMContentLoaded', function() {
+    var checkbox = document.getElementById('checkbox');
+
+    checkbox.addEventListener('click', function() {
+        if(checkbox.checked){
+            document.getElementById("start-time").value = "start";
+            document.getElementById("end-time").value = "end";
+
+            document.getElementById("start-time").readOnly = true;
+            document.getElementById("end-time").readOnly = true;
+        }else{
+            document.getElementById("start-time").value = "";
+            document.getElementById("end-time").value = "";
+
+            document.getElementById("start-time").readOnly = false;
+            document.getElementById("end-time").readOnly = false;        
+        }
+    });
+});
